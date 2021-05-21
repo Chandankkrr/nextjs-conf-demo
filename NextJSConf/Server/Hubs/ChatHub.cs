5c9a0b2a8500492133614955bd0e3bb45247f777
chandankkrr <chandankkrr@gmail.com>
2021-05-21 23:40:38 +0700

    Initial commit 🚀

---
+++ NextJSConf/Server/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NextJSConf.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string x, string y, string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Context.ConnectionId, x, y, user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
